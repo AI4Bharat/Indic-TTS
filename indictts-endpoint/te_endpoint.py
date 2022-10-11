33b79bf34d6f7e3bf70513b8854e09f544006fe8
svp19 <svprav999@gmail.com>
2022-10-11 07:42:23 +0000

    extend: TTS endpoint for Hindi and Marathi

--- indictts-endpoint/te_endpoint.py
+++ indictts-endpoint/te_endpoint.py
@@ -11,14 +11,22 @@ from fastapi.responses import Response
 from pydantic import BaseModel
 from TTS.utils.synthesizer import Synthesizer
 
+# class Item(BaseModel):
+#     text: str
+#     lang: str | None = 'te'
 
-langs = ['te']
+
+langs = ['te',       # single-speaker
+         'hi', 'mr'  # multi-speaker
+        ]
 models = {}
 for lang in langs:
+    speakers = 'male_female' if lang != 'te' else 'male'
+    speakers_file = f'saved_models/fastpitch/{lang}/{speakers}/speakers.pth' if lang != 'te' else None
     models[lang] = {}
     models[lang]['synthesizer']  = Synthesizer(
-        tts_checkpoint=f'saved_models/fastpitch/{lang}/male/best_model.pth',
-        tts_config_path=f'saved_models/fastpitch/{lang}/male/config.json',
+        tts_checkpoint=f'saved_models/fastpitch/{lang}/{speakers}/best_model.pth',
+        tts_config_path=f'saved_models/fastpitch/{lang}/{speakers}/config.json',
         tts_speakers_file=None,
         tts_languages_file=None,
         vocoder_checkpoint=f'saved_models/hifigan/{lang}/male_female/best_model.pth',
@@ -31,7 +39,7 @@ for lang in langs:
     print("*"*100)
 
 app = Flask(__name__)
-socketio = SocketIO(app, async_mode='threading', cors_allowed_origins="*", path='tts_socket.io', async_handlers=True, pingTimeout=60000)
+socketio = SocketIO(app, async_mode='eventlet', cors_allowed_origins="*", path='tts_socket.io', async_handlers=True, pingTimeout=60000)
 
 @socketio.on('connect',namespace='/tts')
 def connection(x):
@@ -47,7 +55,8 @@ def infer(req_data):
     if text in [None, ''] or language in [None, '']:
         status = 'ERROR'
         print(traceback.format_exc())
-    wavs = models[language]['synthesizer'].tts(text, speaker_name="", style_wav="")
+    speaker_name = req_data['speaker'] if language != 'te' else ""
+    wavs = models[language]['synthesizer'].tts(text, speaker_name=speaker_name, style_wav="")
     out = io.BytesIO()
     models[language]['synthesizer'].save_wav(wavs, out)
     print(time.time() - start_time, "Total Time elapsed!")
@@ -57,6 +66,7 @@ def infer(req_data):
 # @app.route("/endpoint", methods=["GET", "POST"])
 # def endpoint_http():
 #     return [1, 2, 3]
+
 
 if __name__ == '__main__':
     socketio.run(app, host="0.0.0.0", port=5001, debug=True)
