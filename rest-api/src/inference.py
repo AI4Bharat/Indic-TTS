14e239ff24066e4d769864549fc7864895f313c8
svp19 <svprav999@gmail.com>
2022-10-31 18:53:57 +0530

    add:DCCRN postprocessing

--- rest-api/src/inference.py
+++ rest-api/src/inference.py
@@ -1,8 +1,18 @@
 import io
+import torch
 import base64
+import ffmpeg
+import librosa
+import numpy as np
+import soundfile as sf
+from uuid import uuid4
+
 
 from TTS.utils.synthesizer import Synthesizer
+from asteroid.models import BaseModel as AsteroidBaseModel
 from aksharamukha.transliterate import process as aksharamukha_xlit
+from scipy.io.wavfile import write as scipy_wav_write
+
 
 from src.models.common import Language
 from src.models.request import TTSRequest
@@ -14,6 +24,10 @@ class TextToSpeechEngine:
         if allow_transliteration:
             from ai4bharat.transliteration import XlitEngine
             self.xlit_engine = XlitEngine(list(models), beam_width=6)
+
+        self.orig_sr = 22050
+        self.target_sr = 16000
+        self.postprocessor = AsteroidBaseModel.from_pretrained("JorisCos/DCCRNet_Libri1Mix_enhsingle_16k")
         
 
     def infer_from_request(self, request: TTSRequest, transliterate_roman_to_indic: bool = True):
@@ -40,8 +54,19 @@ class TextToSpeechEngine:
                 input_text = aksharamukha_xlit("MeeteiMayek", "Bengali", input_text)
             
             wav_obj = model.tts(input_text, speaker_name=gender, style_wav="")
+            
+            # Denoiser
+            wav = np.array(wav_obj)
+            if len(wav.shape) > 1:
+                wav = np.mean(wav, axis=1)
+            wav = librosa.resample(wav, orig_sr=self.orig_sr, target_sr=self.target_sr)
+            wav = torch.Tensor(wav.reshape(1, 1, wav.shape[0])).float()
+            wav = self.postprocessor.separate(wav)[0][0] #(batch, channels, time) -> (time)
+
+
             byte_io = io.BytesIO()
-            model.save_wav(wav_obj, byte_io)
+            scipy_wav_write(byte_io, self.target_sr, wav.cpu().detach().numpy())
+            # model.save_wav(wav, byte_io)
             encoded_bytes = base64.b64encode(byte_io.read())
             encoded_string = encoded_bytes.decode()
             speech_response = AudioFile(audioContent=encoded_string)
