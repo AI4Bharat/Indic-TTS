2750707b5209bb2eea04d809e0670ec35bb81aa2
svp19 <svprav999@gmail.com>
2022-11-08 13:33:08 +0530

    add: enable CUDA

--- rest-api/src/postprocessor/postprocessor.py
+++ rest-api/src/postprocessor/postprocessor.py
@@ -1,4 +1,5 @@
 import os
+import time
 import torch
 import ffmpeg
 import librosa
@@ -16,7 +17,8 @@ class PostProcessor:
     def __init__(self, orig_sr:int, target_sr:int):
         self.orig_sr = orig_sr
         self.target_sr = target_sr
-        self.denoiser = AsteroidBaseModel.from_pretrained("JorisCos/DCCRNet_Libri1Mix_enhsingle_16k")
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.denoiser = AsteroidBaseModel.from_pretrained("JorisCos/DCCRNet_Libri1Mix_enhsingle_16k").to(self.device)
         self.vad = VoiceActivityDetection()
         os.makedirs('wavs', exist_ok=True)  # tmp directory for ffmpeg processing
 
@@ -40,11 +42,12 @@ class PostProcessor:
         if len(wav.shape) > 1:
             wav = np.mean(wav, axis=1)
         wav = librosa.resample(wav, orig_sr=self.orig_sr, target_sr=self.target_sr)
-        wav = torch.Tensor(wav.reshape(1, 1, wav.shape[0])).float()
+        wav = torch.Tensor(wav.reshape(1, 1, wav.shape[0])).float().to(self.device)
         wav = self.denoiser.separate(wav)[0][0] #(batch, channels, time) -> (time)
         return wav.cpu().detach().numpy()
 
     def process(self, wav_obj:list, lang:str, gender:str):
+        st = time.time()
         wav = np.array(wav_obj)
         
         # Denoiser
@@ -56,5 +59,6 @@ class PostProcessor:
         elif (lang == 'mr') and (gender=='female'):  # Marathi female speaker speed up
             wav = self.trim_silence(wav)
             wav = self.set_tempo(wav, '1.15')
-
+        et = time.time()
+        print('Elapsed: ', et - st, 's')
         return wav
