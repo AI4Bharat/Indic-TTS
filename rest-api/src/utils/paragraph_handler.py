25b96eea0c9f6945db2ece15809b82111fbfa8a1
svp19 <svprav999@gmail.com>
2023-02-03 17:37:57 +0000

    fix: edge cases

--- rest-api/src/utils/paragraph_handler.py
+++ rest-api/src/utils/paragraph_handler.py
@@ -8,19 +8,20 @@ class ParagraphHandler():
     
     def split_text(self, text:str, delimiter='.'):
         '''Splits text at delimiter into paragraphs of max. length self.L'''
+        delimiter = ' ' if delimiter not in text else delimiter
         if delimiter not in text:
-            delimiter = None
+            return [text]
         paragraphs = []
         l_pos, r_pos = 0, 0
         while r_pos < len(text):
             r_pos = l_pos + self.L
-            if r_pos >= len(text):
-                paragraphs.append(text[l_pos:r_pos])
+            if r_pos >= len(text): # append last paragraph. 
+                paragraphs.append(text[l_pos:len(text)])
                 break
-            while delimiter is not None and text[r_pos] != delimiter:
+            while delimiter is not None and text[r_pos] != delimiter and r_pos > l_pos and r_pos > 0: # find nearest delimiter < r_pos to split paragraph at.
                 r_pos -= 1 
             paragraphs.append(text[l_pos:r_pos+1])
-            l_pos = r_pos + 1
+            l_pos = r_pos + 1  # handle next paragraph
         return paragraphs
 
 
